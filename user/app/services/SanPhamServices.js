73f545db019695fb96e1ee431a4cb3e688a91c14
TrungMai <anh.maitrung@gmail.com>
2022-11-26 14:38:14 +0700

    change data link

--- user/app/services/SanPhamServices.js
+++ user/app/services/SanPhamServices.js
@@ -1,10 +1,10 @@
 function SanPhamServices(){
-    this.url = "https://63676ecaf5f549f052d54efc.mockapi.io";
+    this.url = "https://636a3113b10125b78fd342a4.mockapi.io";
     
     this.danhSachSP = function(){
         return axios({
             method: 'get',
-            url: `${this.url}/danhsachsp`,
+            url: `${this.url}/Product`,
           })
     }
 }
