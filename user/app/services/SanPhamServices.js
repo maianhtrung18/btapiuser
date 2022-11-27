97de1b2d06a52d7d3e3f4b84cea8525627c53418
Phamtu <phamngoctu2506@gmail.com>
2022-11-27 11:08:27 +0700

    okasda

--- user/app/services/SanPhamServices.js
+++ user/app/services/SanPhamServices.js
@@ -6,5 +6,10 @@ function SanPhamServices(){
             method: 'get',
             url: `${this.url}/danhsachsp`,
           })
+        // return axios({
+        //     method: 'get',
+        //     url: `https://636a3113b10125b78fd342a4.mockapi.io/Product`,
+        
+        // });
     }
 }
